b143a970fb5ee98f7607101a2c6740b6aac63e53
PEMapModder <pemapmodder1970@gmail.com>
2014-05-16 00:09:53 +0800

    rubbish * 1

---
+++ hubapi/pocketmine/Config.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace hubapi\pocketmine;
+
+class Config{
+}
--- hubapi/pocketmine/Main.php
+++ hubapi/pocketmine/Main.php
@@ -1,6 +1,32 @@
 <?php
 
-use pocketmine\pluginp*\PluginBase;
+namespace hubapi\pocketmine;
 
-classM Main extends PlugnBase{
+use hubapi\Minigame;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+	const NAME = "HubAPI";
+	private $mgs;
+	public function onEnable(){ // @shoghicp @$#%$(?:-(??:"*&+!!&"&#$+)&$#
+		$this->mgs = array();
+		$this->getLogger()->log("Waiting for minigames to register to HubAPI");
+		@mkdir($this->getDataFolder());
+		@mkdir($this->mgFolder = $this->getDataFolder()."mgs/");
+		@mkdir($this->pFolder = $this->getDataFolder()."players/");
+		$this->getServer()->getScheduler()->scheduleDelayedTask($this, 5);
+	}
+	public function onRun($t){
+		$config = array();
+		foreach($this->mgs as $name => $mg){
+			$this->getLogger()->log("Loading minigame $name.");
+			$config["minigames"][$name] = $mf->getConfig();
+		}
+		$this->config = new Config($this->getDataFolder()."config.json", $config);
+	}
+	public function register(Minigame $mg){
+		$this->mgs[$mg->getName()] = $mg;
+		return true;
+	}
 }
